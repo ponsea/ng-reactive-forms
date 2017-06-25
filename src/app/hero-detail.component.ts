597d61ac254bc8316bed68f05192457c86d32214
tike <puodsea@gmail.com>
2017-06-25 22:56:46 +0900

    Add more FormControls to HeroDetailComponent

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -1,12 +1,15 @@
 import { Component }   from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
+import { states } from './data-model';
+
 @Component({
   selector: 'hero-detail',
   templateUrl: './hero-detail.component.html'
 })
 export class HeroDetailComponent {
   heroForm: FormGroup;
+  states = states;
 
   constructor(private fb: FormBuilder) {
     this.createForm();
@@ -14,7 +17,15 @@ export class HeroDetailComponent {
 
   createForm() {
     this.heroForm = this.fb.group({
-      name: ['', Validators.required ]
+      name: ['', Validators.required ],
+      address: this.fb.group({
+        street: '',
+        city: '',
+        state: '',
+        zip: ''
+      }),
+      power: '',
+      sidekick: ''
     });
   }
 }
