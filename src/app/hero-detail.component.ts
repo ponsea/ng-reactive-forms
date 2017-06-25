fb4a284e15513062b45b04465b8ed6db6d05db77
tike <puodsea@gmail.com>
2017-06-25 20:21:23 +0900

    Add a FormGroup to HeroDetailComponent

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -1,10 +1,12 @@
 import { Component }   from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'hero-detail',
   templateUrl: './hero-detail.component.html'
 })
 export class HeroDetailComponent {
-  name = new FormControl();
+  heroForm = new FormGroup({
+    name: new FormControl()
+  });
 }
